7c5146ea45a9fe7c9a0c8a3f95b237437e388607
Anne <rocheanne83@gmail.com>
2019-04-04 13:34:09 +0800

    Updates

---
+++ precache-manifest.33d8d5de3c6fd1909641421bf2cac7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d114e1754a179d623aaa",
+    "url": "/static/js/main.d114e175.chunk.js"
+  },
+  {
+    "revision": "262cee88efa7e6e97824",
+    "url": "/static/js/2.262cee88.chunk.js"
+  },
+  {
+    "revision": "d114e1754a179d623aaa",
+    "url": "/static/css/main.722092ea.chunk.css"
+  },
+  {
+    "revision": "262cee88efa7e6e97824",
+    "url": "/static/css/2.a8da185e.chunk.css"
+  },
+  {
+    "revision": "03ac01d7d613049628b34a6da6a67996",
+    "url": "/index.html"
+  }
+];
